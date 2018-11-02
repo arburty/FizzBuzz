66e72e4019ae837aad028761d45ec399d2011802
arburty <austin@burt.us.com>
2018-11-01 18:42:21 -0600

    first try and got it

--- Fizzbuzz.cc
+++ Fizzbuzz.cc
@@ -8,10 +8,34 @@
 
 using namespace std;
 
+ //these member variables will be optional arguments in the future
 Fizzbuzz::Fizzbuzz() : _start(0), _stop(100), _fizz(3), _buzz(5) {
     run();
 }
 
 void Fizzbuzz::run() {
     cout << "-- In run() --\n";
+
+     //for when start and stop are arguments
+    if (_start >= _stop) {
+        cerr << "start must be greater than stop\n";
+        exit(0);
+    }
+
+    int count = _start;
+    while (count++ < _stop) {
+        if (count % _fizz == 0) {
+            if (count % _buzz == 0) {
+                cout << "fizzbuzz\n";
+                continue;
+            }
+            cout << "fizz\n";
+            continue;
+        } else if (count % _buzz == 0) {
+            cout << "buzz\n";
+            continue;
+        }
+        cout << count << "\n";
+    }
+    cout << "--End of Run() --\n";
 }
